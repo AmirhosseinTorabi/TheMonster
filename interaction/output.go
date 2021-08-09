d14f518a4b7870bdca8f29a79752244b603ea791
“Milad <eng.milad.samani@gmail.com>
2021-08-09 12:00:38 +0430

    adding third party application

--- interaction/output.go
+++ interaction/output.go
@@ -3,27 +3,31 @@ package interaction
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+
+	"github.com/common-nighthawk/go-figure"
 )
 
-type RoundData struct{
-	Action string
-	PlayerAttackDmg int
-	PlayerHealValue int
+type RoundData struct {
+	Action           string
+	PlayerAttackDmg  int
+	PlayerHealValue  int
 	MonsterAttackDmg int
-	PlayerHealth int
-	MonsterHealth int
+	PlayerHealth     int
+	MonsterHealth    int
 }
 
 func PrintGreeting() {
+	myFigure := figure.NewFigure("MONSTER", "", true)
+	myFigure.Print()
 	fmt.Println("MONSTER SLAYER")
 	fmt.Println("Starting a new game...")
-	fmt.Println("Good luck!")
 }
 
 func ShowAvailableActions(specialAttackIsAvailable bool) {
 	fmt.Println("=========================")
 	fmt.Println("Please choose your action")
-	fmt.Println("-------------------------")
+	fmt.Println("=========================")
 	fmt.Println("(1) Attack Monster")
 	fmt.Println("(2) Heal")
 	if specialAttackIsAvailable {
@@ -33,25 +37,32 @@ func ShowAvailableActions(specialAttackIsAvailable bool) {
 
 func DeclareWinner(winner string) {
 	fmt.Println("-------------------------")
-	fmt.Println("GAME OVER!")
+	myFigure := figure.NewColorFigure("END GAME", "", "red", true)
+	myFigure.Print()
 	fmt.Println("-------------------------")
-	fmt.Printf("%v won!\n", winner)
+	fmt.Printf("%v WON!\n", winner)
 }
 
-func PrintRoundStatictis(roundData *RoundData)  {
+func PrintRoundStatictis(roundData *RoundData) {
 	if roundData.Action == "ATTACK" {
-		fmt.Printf("Player attacked monster for %v damage!\n " , roundData.PlayerAttackDmg)
+		fmt.Printf("Player attacked monster for %v damage!\n ", roundData.PlayerAttackDmg)
 	} else if roundData.Action == "SPECIAL_ATTACK" {
-		fmt.Printf("Player Performed the Strong Attack for %v damage!\n " , roundData.PlayerAttackDmg)
+		fmt.Printf("Player Performed the Strong Attack for %v damage!\n ", roundData.PlayerAttackDmg)
 	} else {
-		fmt.Printf("Player Healed for : %v \n" , roundData.PlayerHealth)
+		fmt.Printf("Player Healed for : %v \n", roundData.PlayerHealth)
 	}
-	fmt.Printf("Monster Attacked Player for %v damage!\n " , roundData.MonsterAttackDmg)
-	fmt.Printf("Player Health : %v\n " , roundData.PlayerHealth)
-	fmt.Printf("Monster Health : %v \n " , roundData.MonsterHealth)
-} 
+	fmt.Printf("Monster Attacked Player for %v damage!\n ", roundData.MonsterAttackDmg)
+	fmt.Printf("Player Health : %v\n ", roundData.PlayerHealth)
+	fmt.Printf("Monster Health : %v \n ", roundData.MonsterHealth)
+}
 func WriteToFile(rounds *[]RoundData) {
-	file, err := os.Create("gamelog.txt")
+	//file, err := os.Create("gamelog.txt") //for go run
+	pathEX, err := os.Executable()
+	if err != nil {
+		fmt.Println("Writing a log file failed.Exiting.")
+	}
+	pathEX = filepath.Dir(pathEX)
+	file, err := os.Create(pathEX + "/gamelog.txt")
 	if err != nil {
 		fmt.Println("Saving a log file failed. Exiting.")
 		return
@@ -74,5 +85,5 @@ func WriteToFile(rounds *[]RoundData) {
 		}
 	}
 	file.Close()
-	fmt.Println("Wrote data to log!")
+	fmt.Println("--Wrote Data To Log!--")
 }
